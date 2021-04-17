172b9de7e5b2ad7a3aa1b153b4049592c7733c90
RongyeL <82352105+RongyeL@users.noreply.github.com>
2021-04-17 22:13:50 +0800

    Update div5.v

--- 4 div5/div5.v
+++ 4 div5/div5.v
@@ -21,7 +21,7 @@ module DIV5(
         coutnege    // negedge of counter
 );  
     // N = 5
-    // div1 £Ω (N - 1) / 2, div2 = N - 1
+    // div1 Ôºù (N - 1) / 2, div2 = N - 1
     parameter div1 = 2 , div2 = 4; 
 
     input              Clk;
@@ -46,46 +46,46 @@ module DIV5(
     always@(posedge Clk or negedge rst_n)// posedge trigger 
     begin    
         if(!rst_n)  
-            clk_pose = 0;    
+            clk_pose <= 0;    
         else if(coutpose == div1)    
-            clk_pose = ~clk_pose;    
+            clk_pose <= ~clk_pose;    
         else if(coutpose == div2)    
-            clk_pose = ~clk_pose;    
+            clk_pose <= ~clk_pose;    
         else    
-            clk_pose = clk_pose;    
+            clk_pose <= clk_pose;    
     end
            
     always@(negedge Clk or negedge rst_n)// negedge trigger     
     begin    
         if(!rst_n)    
-            clk_nege = 0;    
+            clk_nege <= 0;    
         else if(coutnege == div1)    
-            clk_nege = ~clk_nege;    
+            clk_nege <= ~clk_nege;    
         else if(coutnege == div2)    
-            clk_nege = ~clk_nege;    
+            clk_nege <= ~clk_nege;    
         else    
-            clk_nege = clk_nege;    
+            clk_nege <= clk_nege;    
     end   
     
     // Counter 2     
     always@(posedge Clk or negedge rst_n)// posedge trigger   
     begin    
         if(!rst_n)    
-            coutpose = 0;    
+            coutpose <= 0;    
         else if(coutpose == div2)    
-            coutpose = 0;    
+            coutpose <= 0;    
         else    
-            coutpose = coutpose + 1;    
+            coutpose <= coutpose + 1;    
     end    
          
     always@(negedge Clk or negedge rst_n)// negedge trigger      
     begin    
         if(!rst_n)    
-            coutnege = 0;    
+            coutnege <= 0;    
         else if(coutnege == div2)    
-            coutnege = 0;    
+            coutnege <= 0;    
         else    
-            coutnege = coutnege + 1;    
+            coutnege <= coutnege + 1;    
     end    
     
     // Get 50% duty cycle through OR operation
